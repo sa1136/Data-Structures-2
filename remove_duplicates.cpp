a283138b876b1ead2d722dc546bda17f2649fb6a
sa1136 <spakhilaa10@gmail.com>
2025-11-25 11:55:08 -0500

    Add remove duplicates solution

---
+++ remove_duplicates.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    // Removes duplicates from sorted array in-place and returns new length
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+
+        int i = 0;  // Slow pointer that tracks the position of last unique element
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];  // Place next unique value after the last unique
+            }
+        }
+        return i + 1;  // Length is last unique index + 1
+    }
+};
+
+int main() {
+    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    Solution s;
+    int k = s.removeDuplicates(nums);
+
+    cout << "k = " << k << endl;
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
